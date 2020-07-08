6cb4a3981ac18971cef7245b3bd06acbf9585aa8
moinakash <moinakashll@gmail.com>
2020-07-08 11:46:15 +0600

    home complete

--- app/src/main/java/com/example/islamicappb/HomeFragment.java
+++ app/src/main/java/com/example/islamicappb/HomeFragment.java
@@ -1,7 +1,12 @@
 package com.example.islamicappb;
 
+import android.content.res.Configuration;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,21 +14,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.time.LocalDate;
+import java.time.chrono.HijrahChronology;
+import java.time.chrono.HijrahDate;
+import java.util.Locale;
+
+
 public class HomeFragment extends Fragment {
 
     TextView tvfojor, tvjohor, tvasor, tvmagrib, tvesha;
+    TextView tvCurrentNTime, tvCurrentDate,tvCurrentArbiDate;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    String text;
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -31,15 +39,7 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -59,6 +59,7 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -66,11 +67,83 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+
+
         tvfojor = view.findViewById(R.id.idFajr);
         tvjohor = view.findViewById(R.id.idJohor);
         tvasor = view.findViewById(R.id.idAsor);
         tvmagrib = view.findViewById(R.id.idMagrib);
         tvesha = view.findViewById(R.id.idIsha);
+
+        tvCurrentNTime = view.findViewById(R.id.idCurrentNamajTime);
+        tvCurrentDate = view.findViewById(R.id.idCurrentDate);
+        tvCurrentArbiDate = view.findViewById(R.id.idCurrentDateArbi);
+
+        Locale locale = new Locale("bn");
+        Locale.setDefault(locale);
+//        Configuration config =
+//                getContext().getResources().getConfiguration();
+//        config.setLocale(locale);
+//
+//        createConfigurationContext(config);
+        Calendar cl = Calendar.getInstance(locale);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMMM yyyy ");
+        String dateTime = simpleDateFormat.format(cl.getTime());
+
+
+
+        HijrahDate islamyDate = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            islamyDate = HijrahChronology.INSTANCE.date(LocalDate.of(cl.get(Calendar.YEAR),cl.get(Calendar.MONTH)+1, cl.get(Calendar.DATE)));
+            text = islamyDate.toString();
+            text = text.replace("Hijrah-umalqura AH", "");
+
+
+
+
+            ////////////////////////////////////////////////////////////////////////////////////
+
+            ///////////////////////////////////////////////////////////////////////////
+
+
+            text = text.replace("0", "০");
+            text = text.replace("1", "১");
+            text = text.replace("2", "২");
+            text = text.replace("3", "৩");
+            text = text.replace("4", "৪");
+            text = text.replace("5", "৫");
+            text = text.replace("6", "৬");
+            text = text.replace("7", "৭");
+            text = text.replace("8", "৮");
+            text = text.replace("9", "৯");
+
+
+            //////////////////////////////////////////////////////
+
+            text = text.replace("-০১-", ",মহরম,");
+            text = text.replace("-০2-", "সফল");
+            text = text.replace("-০৩-", "রবিউল আউয়াল");
+            text = text.replace("-০৪-", "রবিউস সানি");
+            text = text.replace("-০৫-", "জমাদিউল আউয়াল");
+            text = text.replace("-০৬-", "জমাদিউস সানি");
+            text = text.replace("-০৭-", "রজব");
+            text = text.replace("-০৮-", "শাবান");
+            text = text.replace("-০৯-", "রমজান");
+            text = text.replace("-১০-", "শওয়াল");
+            text = text.replace("-১১-", ",জিলক্বদ,");
+            text = text.replace("-১২-", "জিলহজ্জ");
+            tvCurrentArbiDate.setText(text+" হিঃ");
+
+        }
+
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.O) {
+            text = "Its only for android orio";
+           tvCurrentArbiDate.setVisibility(View.GONE);
+
+        }
+
+
+
 
         MainActivity activity = (MainActivity) getActivity();
         String fojorerTime = activity.fojorData();
@@ -79,11 +152,19 @@ public class HomeFragment extends Fragment {
         String magriberTime = activity.magribData();
         String esharTime = activity.eshaData();
 
+        String CurrentNamajTime = activity.currentNamajTime();
+
+
         tvfojor.setText(fojorerTime);
         tvjohor.setText(johorerTime);
         tvasor.setText(asorerTime);
         tvmagrib.setText(magriberTime);
         tvesha.setText(esharTime);
+
+
+        tvCurrentNTime.setText(""+CurrentNamajTime);
+        tvCurrentDate.setText(dateTime);
+      //  tvCurrentArbiDate.setText(text);
 
         return view;
 
--- app/src/main/java/com/example/islamicappb/MainActivity.java
+++ app/src/main/java/com/example/islamicappb/MainActivity.java
@@ -11,6 +11,9 @@ import android.util.Log;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     ChipNavigationBar BottomNav;
@@ -18,10 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    final String myString = "hello";
+//    final String myString = "hello";
      String myString2;
 
-    String fajarTime, johorTime, asorTime, magribTime, eshaTime, sunriseTime;
+    String fajarTime, johorTime, asorTime, magribTime, eshaTime, sunriseTime, nextnamaj;
+    String fajarTimeR, johorTimeR, asorTimeR, magribTimeR, eshaTimeR, sunriseTimeR, nextnamajR;
+
+    int cTime, fTime, jTime, aTime, mTime, eTime, sTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,90 @@ public class MainActivity extends AppCompatActivity {
         magribTime = getIntent().getExtras().getString("magrib");
         eshaTime = getIntent().getExtras().getString("esha");
         sunriseTime = getIntent().getExtras().getString("sunrise");
+
+
+        fajarTimeR = fajarTime.replace(":", "");
+        fTime = Integer.parseInt(fajarTimeR);
+        fTime= fTime/100;
+
+
+        johorTimeR = johorTime.replace(":", "");
+        jTime = Integer.parseInt(johorTimeR);
+        jTime= jTime/100;
+
+
+        asorTimeR = asorTime.replace(":", "");
+        aTime = Integer.parseInt(asorTimeR);
+        aTime= aTime/100;
+
+
+        magribTimeR = magribTime.replace(":", "");
+        mTime = Integer.parseInt(magribTimeR);
+        mTime= mTime/100;
+
+
+        eshaTimeR = eshaTime.replace(":", "");
+        eTime = Integer.parseInt(eshaTimeR);
+        eTime= eTime/100;
+
+        sunriseTimeR = sunriseTime.replace(":", "");
+        sTime = Integer.parseInt(sunriseTimeR);
+        sTime= eTime/100;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH mm ");
+
+
+        String dateTime = simpleDateFormat.format(calendar.getTime());
+
+
+        dateTime = dateTime.replace(" ", "");
+
+
+        cTime = Integer.parseInt(dateTime);
+
+
+
+
+        if (cTime>fTime && cTime<sTime){
+            nextnamaj = "Fojor namaj";
+        }
+        if (cTime>sTime && cTime<jTime){
+
+            nextnamaj = "poroborti namaj johor";
+        }
+        if (cTime>=jTime && cTime<(aTime-30)){
+            nextnamaj = "Johor namaj";
+        }
+        if (cTime>=(aTime-30) && cTime<aTime){
+            nextnamaj = "poroborti namaj asor";
+        }
+        if (cTime>=aTime && cTime<(mTime-20)){
+            nextnamaj = "Asor namaj";
+        }
+        if (cTime>=(mTime-20) && cTime<mTime){
+            nextnamaj = "poroborti namaj Magrib";
+        }
+
+        if (cTime>=mTime && cTime<(mTime+30)){
+            nextnamaj = "Magrib namaj";
+        }
+        if (cTime>=(mTime+30) && cTime<eTime){
+             nextnamaj = "poroborti namaj Esha";
+        }
+        if (cTime>=eTime && cTime<2345){
+            nextnamaj = "Esha namaj";
+        }
+        if (cTime>=2345 && cTime<2359){
+
+            nextnamaj = "poroborti namaj fojor";
+        }
+        if (cTime>=0 && cTime<fTime){
+
+            nextnamaj = "poroborti namaj fojor";
+        }
 
 
 
@@ -104,6 +194,11 @@ public class MainActivity extends AppCompatActivity {
     public String eshaData() {
         return eshaTime;
     }
+    public String currentNamajTime() {
+        return nextnamaj;
+    }
+
+
 
 
 }
