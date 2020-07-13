fce1e4fe43d41ff196ab88dd14524304a3ecb6e8
Farhad <imfarhad01@gmail.com>
2020-07-13 15:56:32 +0600

    db modified 1

--- app/src/main/java/com/example/islamicappb/TimeTableFragment.java
+++ app/src/main/java/com/example/islamicappb/TimeTableFragment.java
@@ -332,8 +332,7 @@ public class TimeTableFragment extends Fragment {
         } else if (matchString.equals("এশা")){
             tvNamajTimeNUmber.setText(""+eshaTimeBangla);
         }
-
-
+        
 
         return view;
 
