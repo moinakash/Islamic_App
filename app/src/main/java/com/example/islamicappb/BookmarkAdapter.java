3d5c2954d181ef40ae8e25b3095ef4a1d06d5790
moinakash <moinakashll@gmail.com>
2020-07-22 18:23:45 +0600

    Bookmark Adapter & pojo class added error

--- app/src/main/java/com/example/islamicappb/BookmarkAdapter.java
+++ app/src/main/java/com/example/islamicappb/BookmarkAdapter.java
@@ -39,6 +39,8 @@ public class BookmarkAdapter extends ArrayAdapter<BookmarkPojoClass> {
         }
 
 
+        
+
 
         BookmarkPojoClass bookmarkPojoClass1 =bookmarkPojoClasses.get(position);
 
