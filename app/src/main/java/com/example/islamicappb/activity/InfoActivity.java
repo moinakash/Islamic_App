487cabd0e88fbfe29d0424a55514339b9c38e078
moinakash <moinakashll@gmail.com>
2020-08-17 09:25:30 +0600

    sura activity fixed

---
+++ app/src/main/java/com/example/islamicappb/activity/InfoActivity.java
@@ -0,0 +1,20 @@
+package com.example.islamicappb.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.islamicappb.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+
+
+
+    }
+}
